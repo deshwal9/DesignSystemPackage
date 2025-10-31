9cd7f963535917501d7e4db15eb7b9f8d1ede7a5
Ankit Kumar <Ankit9deshwal9@gmail.com>
2025-10-31 16:01:21 +0000

    update code with sample testing

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "DesignSystem",
+    name: "NHSDesignSystem",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DesignSystem",
+            name: "NHSDesignSystem",
             targets: ["DesignSystem"]),
-        
+        .library(name: "NHSDesignSystem", type: .dynamic, targets: ["DesignSystem"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
